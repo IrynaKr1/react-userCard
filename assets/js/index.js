2dace22af23b67b28d733a4ae1a7b293e866afa0
IrynaKr1 <krutsiryna@gmail.com>
2025-08-31 18:16:54 +0200

    Add react component with userInfo

---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+function App() {
+  const user = {
+    imgSrc:
+      'https://media.themoviedb.org/t/p/w500/A14lLCZYDhfYdBa0fFRpwMDiwRN.jpg',
+    name: 'Emma Watson',
+    nick: '@EmWatson',
+    tweetsQty: 1337,
+    folowing: 561,
+    folowers: 718,
+  };
+  return (
+    <>
+      <UserCard
+        userImg={user.imgSrc}
+        userName={user.name}
+        userNick={user.nick}
+        userTweetQty={user.tweetsQty}
+        userFolowing={user.folowing}
+        userFolowers={user.folowers}
+      />
+    </>
+  );
+}
+
+function UserCard({
+  userImg,
+  userName,
+  userNick,
+  userTweetQty,
+  userFolowing,
+  userFolowers,
+}) {
+  return (
+    <article className="userCard">
+      <img src={userImg} alt={userName} className="userImg"/>
+      <h2 className="userName">{userName}</h2>
+      <p className="userNick">{userNick}</p>
+      <button className="addButton">+</button>
+      <p className="userTweetQty">{userTweetQty}</p>
+      <p className="userFolowing">{userFolowing}</p>
+      <p className="userFolowers">{userFolowers}</p>
+    </article>
+  );
+}
